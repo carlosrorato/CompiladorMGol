5bf18949cee4a0b0b94d196d99512b1515664e6c
Carlos Henrique <carlosrorato2014@gmail.com>
2019-04-10 19:28:26 -0300

    Tabela DFA até o estado 6

--- Projeto/compilador.py
+++ Projeto/compilador.py
@@ -17,8 +17,8 @@ def preenche_tabela_dfa():
         linha.update({c:9})
     for c in string.ascii_uppercase:
         linha.update({c: 9})
-    for c in range(0,9):
-        linha.update({c:1})
+    for c in range(0,10):
+        linha.update({str(c):1})
     linha.update({"\n":0 , "\_":0 , "\t": 0})
     linha.update({"EOF":13})
     linha.update({"\"": 7})
@@ -27,6 +27,50 @@ def preenche_tabela_dfa():
     linha.update({"(": 20, ")": 21, ";": 22})
     Tabela_Transicao.append(linha)
 
+    # preenchimento para o estado 1:
+    linha = {}
+    for c in range(0, 10):
+        linha.update({str(c): 1})
+    linha.update({".": 2, "E": 4, "e": 4})
+    Tabela_Transicao.append(linha)
+
+    # preenchimento para o estado 2:
+    linha = {}
+    for c in range(0, 10):
+        linha.update({str(c): 3})
+    Tabela_Transicao.append(linha)
+
+    # preenchimento para o estado 3:
+    linha = {}
+    for c in range(0, 10):
+        linha.update({str(c): 3})
+    linha.update({"E": 4, "e": 4})
+    Tabela_Transicao.append(linha)
+
+    # preenchimento para o estado 4:
+    linha = {}
+    for c in range(0, 10):
+        linha.update({str(c): 6})
+    linha.update({"+": 5, "-": 5})
+    Tabela_Transicao.append(linha)
+
+    # preenchimento para o estado 5:
+    linha = {}
+    for c in range(0, 10):
+        linha.update({str(c): 6})
+    Tabela_Transicao.append(linha)
+
+    # preenchimento para o estado 6:
+    linha = {}
+    for c in range(0, 10):
+        linha.update({str(c): 6})
+    Tabela_Transicao.append(linha)
+
 
 preenche_tabela_dfa()
-print(Tabela_Transicao)
+
+#Imprime toda a tabela de transições
+i = 0
+for k in Tabela_Transicao:
+    print(Tabela_Transicao[i])
+    i+=1
