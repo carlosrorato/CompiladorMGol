e2556987078ba3c982be0735d9d511e595d5213d
Carlos Henrique <carlosrorato2014@gmail.com>
2019-07-02 00:13:55 -0300

    nome do arquivo .c

--- Projeto/main.py
+++ Projeto/main.py
@@ -19,6 +19,7 @@ from Lexico.analisadorLexico import *
 from Sintatico.analisadorSintatico import *
 from Sintatico.preencheTabelas import *
 from Semantico.analisadorSemantico import *
+import os
 
 # Preenche as tabelas utilizadas pelo Analisador Sintático
 tabelaAcoes = preencheTabelaAcoes()
@@ -31,7 +32,12 @@ argumentos = sys.argv
 # arqFonte = open(argumentos[1], encoding="utf-8")
 arqFonte = open("fonte.alg", encoding="utf-8")
 
+#separando o nome do arquivo passado como argumento em nome e extensao,
+#para gerar o arquivo .c com o mesmo nome do arquivo de entrada:
+#nome, extensao = os.path.splitext(argumentos[1])
+nome, extensao = os.path.splitext("fonte.alg")
+
 # Chama o Analisador Sintático
-analisadorSintatico(tabelaAcoes,tabelaDesvios,tabelaQtdSimbolos, tabelaErros, arqFonte, "prog")
+analisadorSintatico(tabelaAcoes,tabelaDesvios,tabelaQtdSimbolos, tabelaErros, arqFonte, nome)
 
 arqFonte.close()
